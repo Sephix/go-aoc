b8c3c5e656e4da8ff29357d8596fea88f667dc5d
Michael Schuster <michael.schuster@qiminfo.ch>
2023-08-17 21:08:45 +0200

    day2 refactor

--- internal/day2/day2.go
+++ internal/day2/day2.go
@@ -6,37 +6,40 @@ import (
 	"os"
 )
 
-var input string = "/assets/day2input.txt"
-
-var player1 map[string]int = map[string]int{
-	"A": 1,
-	"B": 2,
-	"C": 3,
-}
-
-var player2 map[string]int = map[string]int{
-	"X": 1,
-	"Y": 2,
-	"Z": 3,
-}
-
-var outcome map[string]int = map[string]int{
-	"AX": 0,
-	"AY": -1,
-	"AZ": 1,
-	"BX": 1,
-	"BY": 0,
-	"BZ": -1,
-	"CX": -1,
-	"CY": 1,
-	"CZ": 0,
-}
-
-var strategy map[string]int = map[string]int{
-	"Y": 3,
-	"X": 0,
-	"Z": 6,
-}
+var (
+	input   string         = "/assets/day2test.txt"
+	player1 map[string]int = map[string]int{
+		"A": 1,
+		"B": 2,
+		"C": 3,
+	}
+	player2 map[string]int = map[string]int{
+		"X": 1,
+		"Y": 2,
+		"Z": 3,
+	}
+	outcome map[string]int = map[string]int{
+		"AX": 0,
+		"AY": -1,
+		"AZ": 1,
+		"BX": 1,
+		"BY": 0,
+		"BZ": -1,
+		"CX": -1,
+		"CY": 1,
+		"CZ": 0,
+	}
+	strategy map[string]int = map[string]int{
+		"Y": 0,
+		"X": 1,
+		"Z": -1,
+	}
+	score map[int]int = map[int]int{
+		0:  3,
+		1:  0,
+		-1: 6,
+	}
+)
 
 func PrintResult() {
 	fmt.Println("--- Day 2 ---")
@@ -63,42 +66,24 @@ func parseInput() (int, int) {
 		p1 := string(currentLine[0])
 		p2 := string(currentLine[2])
 		result1 += playRound(p1, p2)
-		result2 += playStrategie(p1, p2)
+		result2 += playStrategy(p1, p2)
 	}
-
 	return result1, result2
 }
 
 func playRound(p1 string, p2 string) int {
 	result := player2[p2]
-
-	if player1[p1] == player2[p2] {
-		result += 3
-	} else if outcome[p1+p2] == 1 {
-		result += 0
-	} else {
-		result += 6
-	}
-
+	result += score[outcome[p1+p2]]
 	return result
 }
 
-func playStrategie(p1 string, strat string) int {
+func playStrategy(p1 string, strat string) int {
 	currentStrategy := strategy[strat]
-	possibleMove := []string{"X", "Y", "Z"}
+	fmt.Println(p1, currentStrategy)
 	var p2 string
-	for _, val := range possibleMove {
-		if currentStrategy == 0 && outcome[p1+val] == 1 {
-			p2 = val
-			break
-		}
-		if currentStrategy == 3 && outcome[p1+val] == 0 {
-			p2 = val
-			break
-		}
-		if currentStrategy == 6 && outcome[p1+val] == -1 {
-			p2 = val
-			break
+	for key := range player2 {
+		if outcome[p1+key] == currentStrategy {
+			p2 = key
 		}
 	}
 	return playRound(p1, p2)
