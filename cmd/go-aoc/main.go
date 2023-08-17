5b4ad2f28bc06e9602e2c72729400b9fe70efd68
Michael Schuster <michael.schuster@qiminfo.ch>
2023-08-17 19:15:55 +0200

    end day 1

--- cmd/go-aoc/main.go
+++ cmd/go-aoc/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/sephix/go-aoc/internal/day1"
 )
 
 func main() {
-	day1Result := day1.Result()
-	fmt.Println(day1Result)
+	day1.PrintResult()
+
 }
--- internal/day1/day1.go
+++ internal/day1/day1.go
@@ -2,26 +2,28 @@ package day1
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
 
 var input string = "/assets/day1input.txt"
 
-func Result() int {
-
-	return parseInput()
+func PrintResult() {
+	best3 := parseInput()
+	fmt.Printf("Day 1 part1: %d\n", best3[0])
+	fmt.Printf("Day 1 part2: %d\n", sum(best3))
 }
 
-func parseInput() int {
-	result := 0
+func parseInput() []int {
+	result := make([]int, 3)
 	array := make([]int, 1)
 
 	path, _ := os.Getwd()
 	file, err := os.Open(path + input)
 	if err != nil {
 		println(err.Error())
-		return 0
+		return nil
 	}
 
 	fileScanner := bufio.NewScanner(file)
@@ -30,18 +32,35 @@ func parseInput() int {
 	for fileScanner.Scan() {
 		currentLine := fileScanner.Text()
 		if currentLine == "" {
-			if array[len(array)-1] > result {
-				result = array[len(array)-1]
-			}
+			updateResult(result, array)
 			array = append(array, 0)
 		} else {
 			lineVale, _ := strconv.Atoi(fileScanner.Text())
 			array[len(array)-1] = array[len(array)-1] + lineVale
 		}
 	}
-	if array[len(array)-1] > result {
-		result = array[len(array)-1]
-	}
+	updateResult(result, array)
 
+	fmt.Println(result)
+	return result
+}
+
+func updateResult(currentVal []int, array []int) {
+	for i, val := range currentVal {
+		if array[len(array)-1] > val {
+			for j := 2; j > i; j-- {
+				currentVal[j] = currentVal[j-1]
+			}
+			currentVal[i] = array[len(array)-1]
+			break
+		}
+	}
+}
+
+func sum(array []int) int {
+	result := 0
+	for _, v := range array {
+		result += v
+	}
 	return result
 }
